b2b366e48f5d1f5428595882515685e78249b33d
ignavinuales <vinualesignacio@gmail.com>
2023-01-29 19:22:32 -0300

    update training.py

--- training.py
+++ training.py
@@ -11,18 +11,15 @@ import pickle
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
+# Import data
+df = pd.read_csv('Dataset/measuring_hate_speech.csv')
 
-df = pd.read_csv('measuring_hate_speech.csv')
-
+# Clean data
 df['clean_text'] = df['text'].apply(clean_data)
 
 # Split into features and target values
 features = np.array(df['clean_text'].values)
 labels = np.array(df['hatespeech'].values)
-
-# # Encode labels --> 0 = negative, 1 = positive
-# encoder = LabelBinarizer()
-# labels = encoder.fit_transform(labels)
 
 # Tokenazation
 vocab_size = 10000
@@ -48,28 +45,22 @@ print(f'X_test shape: {X_test.shape}')
 print(f'y_train shape: {y_train.shape}')
 print(f'y_test shape: {y_test.shape}')
 
-
+# Reshape labels 
 y_train, y_test = y_train.astype('float32').reshape(-1,1), y_test.astype('float32').reshape(-1, 1)
 
 
 # Model
-model = 0
 tf.keras.backend.clear_session()
 model = Sequential()
 model.add(Embedding(vocab_size, 32, input_length=max_length))
 model.add(SpatialDropout1D(0.3))
-# While passing output to next LSTM layer set return_sequence to Ture.
 model.add(Bidirectional(LSTM(120, return_sequences=True)))
 model.add(Bidirectional(LSTM(64)))
 model.add(Dropout(0.25))
 model.add(Dense(64,activation='relu'))
 model.add(Dense(32,activation='relu'))
 model.add(Dense(10,activation='relu'))
-
-
-# model.add(Flatten())
 model.add(Dense(3,activation=tf.keras.activations.softmax))
-# model.summary()
 
 batch_size = 64
 epochs = 10
@@ -80,7 +71,7 @@ early_stop = callbacks.EarlyStopping(monitor='val_loss',
                                      mode='auto',
                                      restore_best_weights=True)
 
-checkpoint = callbacks.ModelCheckpoint(filepath='checkpoints/model_1_checkpoint.h5', monitor='val_loss', verbose=0, save_best_only=True, save_freq='epoch')
+checkpoint = callbacks.ModelCheckpoint(filepath='Models/LSTM_checkpoint.h5', monitor='val_loss', verbose=0, save_best_only=True, save_freq='epoch')
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=batch_size, callbacks=[early_stop, checkpoint])
